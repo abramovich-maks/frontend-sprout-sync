1fa9ca897acbe6e4d9b202d5bfaa83dfbbe11c95
abramovich-maks <maks.abramovich.1995@mail.ru>
2025-11-30 16:02:10 +0100

    Adding navbar and home page, adding base styles

---
+++ src/Navbar.js
@@ -0,0 +1,15 @@
+const Navbar
+    = () => {
+        return (
+            <nav className="navbar">
+                <h1>The SproutSync</h1>
+                <div className="links">
+                    <a href="/">Home</a>
+                    <a href="/login">Login</a>
+                    <a href="/register">Register</a>
+                </div>
+            </nav>
+        );
+    }
+
+export default Navbar;
